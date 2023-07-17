cc54cceb2d0b057b8aa270b60d9bbe1e4b854d92
Eszuri <121676193+Eszuri@users.noreply.github.com>
2023-07-17 15:26:08 +0700

    211111111111111111111111111

---
+++ script.js
@@ -0,0 +1,60 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    document.getElementById("showTimer").style.display = "none";
+    document.getElementById("timer").style.marginLeft = "0%";
+    document.getElementById("timer").style.marginTop = "-130px";
+    document.querySelector("form").style.marginLeft = "-200%";
+    document.getElementById("tumbal1").innerText = document.getElementById("inputJam").value + " Jam,";
+    document.getElementById("tumbal2").innerText = document.getElementById("inputMenit").value + " Menit,";
+    document.getElementById("tumbal3").innerText = document.getElementById("inputDetik").value + " Detik.";
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    document.querySelector("button").style.display = "none";
+    document.getElementById("showTimer").style.display = "flex";
+    let jam = document.querySelector("#inputJam").value;
+    let min = document.querySelector("#inputMenit").value;
+    let det = document.querySelector("#inputDetik").value;
+    let Jam = document.querySelector("#jam")
+    let menit = document.querySelector("#menit")
+    let detik = document.querySelector("#detik")
+    Jam.innerHTML = parseInt(jam) + ",";
+    menit.innerHTML = parseInt(min) + ",";
+    detik.innerHTML = parseInt(det);
+    function mundur() {
+        det -= 1;
+        if (det === -1) {
+            det = 59;
+            min -= 1;
+        }
+        if (min === -1) {
+            min = 59;
+            jam -= 1;
+        }
+        if (jam === -1) {
+            jam = 0;
+        }
+        if (jam < 1 && min < 1 && det < 1) {
+            document.querySelector("button").style.display = "inline-block";
+            clearInterval(stopInterval)
+        }
+        Jam.innerHTML = parseInt(jam) + ",";
+        menit.innerHTML = parseInt(min) + ",";
+        detik.innerHTML = parseInt(det);
+    }
+    const stopInterval = setInterval(mundur, 1000)
+});
+
+
+// style footer
+const foo = document.querySelector("footer");
+foo.innerText = "Created By Eszuri";
+foo.style.fontSize = "20px";
+foo.classList.add("bg-blue-900");
+foo.style.textAlign = "center";
+foo.style.position = "absolute";
+foo.style.left = "50%";
+foo.style.bottom = "0%";
+foo.style.transform = "translateX(-50%)";
+foo.style.borderTop = "2px solid yellow";
+foo.style.width = "95%"
